219489b7a4dc9f3ef2cb715ebddd328e22a224b3
antoniusekoputranto <63001905+antoniusekoputranto@users.noreply.github.com>
2021-07-27 15:54:16 +0700

    database gloeshoes

--- gloeshoes.sql
+++ gloeshoes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 23 Jul 2021 pada 18.14
+-- Waktu pembuatan: 27 Jul 2021 pada 10.52
 -- Versi server: 10.4.19-MariaDB
 -- Versi PHP: 8.0.6
 
@@ -24,6 +24,82 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `description_company` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `link_whatsapp` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `link_facebook` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `link_instagram` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `accounts`
+--
+
+INSERT INTO `accounts` (`id`, `description_company`, `link_whatsapp`, `link_facebook`, `link_instagram`, `created_at`, `updated_at`) VALUES
+(3, 'gloeshoes.leather merupakan perusahaan yang bergerak dibidang sepatu kulit custom.', '85856577395', 'https://www.facebook.com/gloeshoes.leather/', 'https://www.instagram.com/gloeshoes.leather/', '2021-07-26 04:21:51', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `events`
+--
+
+CREATE TABLE `events` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `image_event` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `link` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `events`
+--
+
+INSERT INTO `events` (`id`, `image_event`, `link`, `created_at`, `updated_at`) VALUES
+(3, '1627287160.jpg', NULL, '2021-07-26 01:12:40', '2021-07-26 01:34:34'),
+(4, '1627287291.jpg', 'https://www.instagram.com/gloeshoes.leather/', '2021-07-26 01:14:51', NULL),
+(5, '1627287312.jpg', 'https://www.facebook.com/gloeshoes.leather', '2021-07-26 01:15:12', NULL),
+(6, '1627287346.jpg', 'https://api.whatsapp.com/send?phone=6285856577395', '2021-07-26 01:15:46', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `feeds`
+--
+
+CREATE TABLE `feeds` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `image_feed` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `link` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `feeds`
+--
+
+INSERT INTO `feeds` (`id`, `image_feed`, `link`, `active`, `created_at`, `updated_at`) VALUES
+(7, '1627199337.jpg', 'https://www.instagram.com/p/CRI2iG-LsGg/', 1, '2021-07-25 00:48:57', NULL),
+(8, '1627199400.jpg', 'https://www.instagram.com/p/CRJCTiTrFvy/', 1, '2021-07-25 00:50:00', NULL),
+(9, '1627199458.jpg', 'https://www.instagram.com/p/CRL01BVLhll/', 1, '2021-07-25 00:50:58', NULL),
+(10, '1627199541.jpg', 'https://www.instagram.com/p/CRTUVMersW7/', 1, '2021-07-25 00:52:21', NULL),
+(11, '1627199583.jpg', 'https://www.instagram.com/p/CRTWKrGL58W/', 1, '2021-07-25 00:53:03', NULL),
+(12, '1627199664.jpg', 'https://www.instagram.com/p/CRiSKkhrhWZ/', 1, '2021-07-25 00:54:24', NULL),
+(13, '1627199757.jpg', 'https://www.instagram.com/p/CRn2-MPr5Au/', 1, '2021-07-25 00:55:57', NULL),
+(14, '1627199811.jpg', 'https://www.instagram.com/p/CRqX7FLrN-8/', 1, '2021-07-25 00:56:51', NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `migrations`
 --
 
@@ -39,7 +115,11 @@ CREATE TABLE `migrations` (
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (4, '2021_06_13_070209_create_products_table', 1),
-(5, '2021_06_13_080226_create_productcolourimages_table', 1);
+(5, '2021_06_13_080226_create_productcolourimages_table', 1),
+(6, '2021_07_24_064118_create_feeds_table', 2),
+(7, '2021_07_26_063434_create_events_table', 3),
+(12, '2021_07_26_092521_create_accounts_table', 4),
+(13, '2021_07_26_094508_add_link_instagram_to_accounts_table', 5);
 
 -- --------------------------------------------------------
 
@@ -92,7 +172,7 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `product_name`, `product_type`, `featured`, `product_image`, `price`, `colour`, `heel_height`, `description`, `created_at`, `updated_at`) VALUES
-(9, 'sepatu a', 'boots', 'n', '1627019907.jpg', 550000, 'red', 20, 'sepatu terbaik di tempat kami', '2021-07-22 22:58:27', NULL),
+(9, 'sepatu a', 'boots', 'y', '1627019907.jpg', 550000, 'red', 20, 'sepatu terbaik di tempat kami', '2021-07-22 22:58:27', NULL),
 (10, 'sepatu b', 'heels', 'n', '1627020077.jpg', 600000, 'blue', 21, 'sepatu khusus untuk wanita karir', '2021-07-22 23:01:17', NULL),
 (11, 'sepatu c', 'wedges', 'n', '1627020131.jpg', 650000, 'green', 22, 'sepatu terbaik khusus untuk wanita', '2021-07-22 23:02:11', NULL),
 (12, 'sepatu d', 'flat', 'y', '1627020233.jpg', 700000, 'yellow', 23, 'sepatu baru datang dari luar negeri', '2021-07-22 23:03:53', NULL);
@@ -121,6 +201,24 @@ INSERT INTO `users` (`id`, `nama`, `email`, `phone`, `password`) VALUES
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `feeds`
+--
+ALTER TABLE `feeds`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `migrations`
@@ -158,10 +256,28 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `events`
+--
+ALTER TABLE `events`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT untuk tabel `feeds`
+--
+ALTER TABLE `feeds`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
 -- AUTO_INCREMENT untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT untuk tabel `orders`
@@ -179,7 +295,7 @@ ALTER TABLE `productcolourimages`
 -- AUTO_INCREMENT untuk tabel `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
